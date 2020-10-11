61d9562e3aee41c3b5903ecf0c61bf2bb8c456f6
Diwas Paudel <diwasp1@gmail.com>
2020-10-11 23:12:08 +0545

    navv

--- js/app.js
+++ js/app.js
@@ -9,67 +9,10 @@ $(document).ready(function () {
     autoplaySpeed: 3000,
     cssEase: "ease-out",
   });
-  $(".design-slider-wrapper").slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    cssEase: "ease-out",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          arrows: false,
-
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-  $(".slider-for").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".slider-nav",
-  });
-  $(".slider-nav").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    asNavFor: ".slider-for",
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
 
   let searchIcon = document.getElementById("search-icon");
   let searchBar = document.querySelector(".search-bar");
-  let hamBar = document.querySelector("#ham-bar")
+  let hamBar = document.querySelector("#ham-bar");
 
   searchIcon.addEventListener("click", displaySearch);
 
@@ -86,24 +29,30 @@ $(document).ready(function () {
   window.onclick = function (event) {
     if (event.target != searchBar && event.target != searchIcon) {
       searchBar.style.display = "none";
-    }else if (event.target != hamBar) {
+    } else if (event.target != hamBar) {
       closeNav();
     }
   };
 });
 
 let navWrapper = document.querySelector(".navbar-wrapper");
+let firstNav = document.querySelector(".first-navbar-wrapper");
+let logoContainer = document.querySelector(".logo-container");
 
 window.onscroll = function () {
-
   if(window.pageYOffset >= navWrapper.offsetTop+80){
     // navWrapper.classList.add("nav-sticky");
     navWrapper.style.position = "fixed";
     navWrapper.style.background = "#0e4966";
     navWrapper.style.transition ="all .9s linear";
+    firstNav.style.display = "none";
+    logoContainer.style.height = "60px"
+
   }else{
     navWrapper.style.position = "absolute";
     navWrapper.style.background = "none";
+    firstNav.style.display = "flex";
+    logoContainer.style.height = "90px";
 
     // navWrapper.classList.remove("nav-sticky");
   }
